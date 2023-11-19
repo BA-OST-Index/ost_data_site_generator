ecb43d3c885a21f04b036115e4504bd3a2057aa8
sctop <2094880085@qq.com>
2023-11-19 10:10:14 +0800

    sepreate generation config file & check_static added

--- main_test.py
+++ main_test.py
@@ -1,16 +1,6 @@
 from _main import *
+from _main_config import *
 
-ALL_LANGS = ["zh_cn"]
-LANG_OTHER_PAGE = {
-    "page/{lang}/main_all.html": "main/index.html",
-    "page/{lang}/_index.html": "index.html",
-    "page/{lang}/_about.html": "about.html"
-}
-OTHER_STATIC_PAGE = {
-    "page/index.html": "index.html",
-    "page/404.html": "404.html",
-    "page/zh_cn/_zhcn_technical.html": "zh_cn/zhcn_technical.html"
-}
 ALL_FOLDERS = ["character"]
 
 delete_old_page()
--- prechecker.py
+++ prechecker.py
@@ -1,5 +1,22 @@
 from _main import get_jinja_env, GeneratorTool
+from _main_config import *
 from precheck_script.check_template import check_template
+from precheck_script.check_static import check_static_per_lang, check_static_otehr
 
 env = get_jinja_env()
-check_template(env, GeneratorTool.return_template)
+encounter_exception = False
+
+try:
+    check_template(env, GeneratorTool.return_template)
+except AssertionError:
+    encounter_exception = True
+
+try:
+    check_static_per_lang(env, LANG_OTHER_PAGE)
+except AssertionError:
+    encounter_exception = True
+
+try:
+    check_static_otehr(env, OTHER_STATIC_PAGE)
+except AssertionError:
+    encounter_exception = True
