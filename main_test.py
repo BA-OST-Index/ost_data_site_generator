87ca6396de3b0d313c9f5868ec394bc19391a31b
sctop <2094880085@qq.com>
2024-01-27 15:24:53 +0800

    colorful interpage for story/battle; minor ui optimization

--- main_test.py
+++ main_test.py
@@ -1,18 +1,16 @@
 from _main import *
 from _main_config import *
-from main_tool_2 import generate_tooltip
-
-ALL_FOLDERS = ["character"]
+from main_tool import INIT_TIME
+from main_tool_2 import generate_tooltip, rename_js
 
 delete_old_page()
 
 env = get_jinja_env()
-generate_tooltip(env)
+# generate_tooltip(env)
+rename_js(INIT_TIME)
 
 copy_static_file(True)
-for i in ALL_FOLDERS:
-    generate_page_per_lang(ALL_LANGS, env, ["exported_data", i])
+
+generate_page_per_lang(ALL_LANGS, env, ["exported_data"])
 generate_other_per_lang(ALL_LANGS, env, LANG_OTHER_PAGE)
 generate_static_page(env, OTHER_STATIC_PAGE)
-
-start_server(9000)
